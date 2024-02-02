3d959e090c6217332eab5e6024f63aaf42253346
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-02 08:30:48 -0800

    Javascript coding of the slider part

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('#header-slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
